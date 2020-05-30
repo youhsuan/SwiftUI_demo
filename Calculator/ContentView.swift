f274a6281287715385e2bdf44842c2cfdb752d57
youhsuan <youhsuanyu@kkstream.com>
2020-05-30 23:17:28 +0800

    Refactor VStack to remove duplicate code.
    Refactor VStack to remove duplicate code.

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -20,31 +20,27 @@ struct CalculatorButtonRow : View {
         }
     }
 }
-struct ContentView: View {
+struct CalculatorButtonPad: View {
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip), .command(.percent), .op(.divide)],
+        [.digit(7), .digit(8), .digit(9), .op(.multiply)],
+        [.digit(4), .digit(5), .digit(6), .op(.minus)],
+        [.digit(1), .digit(2), .digit(3), .op(.plus)],
+        [.digit(0), .dot, .op(.equal)]
+    ]
+    
     var body: some View {
         VStack(spacing: 8) {
-            CalculatorButtonRow(row: [
-                .command(.clear), .command(.flip), .command(.percent), .op(.divide)]
-            )
-            CalculatorButtonRow(row: [
-                .digit(7), .digit(8), .digit(9), .op(.multiply)]
-            )
-            CalculatorButtonRow(row: [
-            .digit(4), .digit(5), .digit(6), .op(.minus)]
-            )
-            CalculatorButtonRow(row: [
-                .digit(1), .digit(2), .digit(3), .op(.plus)]
-            )
-            CalculatorButtonRow(row: [
-                .digit(0), .dot, .op(.equal)
-            ])
+            ForEach(pad, id: \.self) { row in
+                CalculatorButtonRow(row: row)
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CalculatorButtonPad()
     }
 }
 
@@ -96,7 +92,10 @@ extension CalculatorButtonItem: Hashable {
         }
     }
     var size: CGSize {
-        CGSize(width: 88, height: 88)
+        if case .digit(let value) = self, value == 0 {
+            return CGSize(width: 88 * 2 + 8, height: 88)
+        }
+        return CGSize(width: 88, height: 88)
     }
     var backgroundColor: Color {
         switch self {
